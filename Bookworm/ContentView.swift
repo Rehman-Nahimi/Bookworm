c42da774ba84b1a1c935aaf6aa1ba8688da12d1b
Rehman Nahimi <rehman2000@live.com>
2023-10-19 13:20:45 +0100

    Trying to add red text for rating values of 1

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -16,7 +16,12 @@ struct ContentView: View {
     
     @State private var showingAddScreen = false
     
-    
+    var isLowRate: Bool {
+        if books.rating(Int16) == 1 {
+            return false
+        }
+        return true
+    }
     var body: some View {
         NavigationView{
             List{
